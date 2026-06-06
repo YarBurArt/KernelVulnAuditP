1c9448703acee09cccb6389bff9cf79290bf6795
Yaroslav <yaroslav.chayk@gmail.com>
2026-06-06 15:31:59 +0500

    Update install_tools.sh
    fix by shallow

--- install_tools.sh
+++ install_tools.sh
@@ -23,12 +23,12 @@ fi
 cd /tmp
 pwd
 
-git clone https://github.com/CISOfy/lynis
+git clone --depth 1 https://github.com/CISOfy/lynis
 echo "CHANGEME in config.py for: /tmp/lynis/lynis"
 echo "Check here https://github.com/CISOfy/lynis/blob/master/README.md"
 
 echo
-git clone https://github.com/The-Z-Labs/linux-exploit-suggester.git
+git clone --depth 1 https://github.com/The-Z-Labs/linux-exploit-suggester.git
 echo "CHANGEME in config.py for: /tmp/linux-exploit-suggester/linux-exploit-suggester.sh" 
 echo "Check here https://github.com/The-Z-Labs/linux-exploit-suggester/blob/master/README.md"
 
@@ -36,7 +36,7 @@ echo
 OUTPUT="${1:-/tmp/linpeas_kernel.sh}"
 echo "Building linpeas with kernel/CVE checks only..."
 # use python peass builder https://github.com/peass-ng/PEASS-ng/blob/master/linPEAS/builder/README.md
-git clone https://github.com/peass-ng/PEASS-ng.git
+git clone --depth 1 https://github.com/peass-ng/PEASS-ng.git
 cd /tmp/PEASS-ng/linPEAS
 python3 -m builder.linpeas_builder \
     --include "kernel,CVE,exploit" \
